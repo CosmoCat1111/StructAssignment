e4521abe22d2e1735907f10914c26bc5a53318a6
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-18 13:32:55 -0700

    Add project files.

---
+++ StructAssignment/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    struct Number //creating struct
+    { 
+        public decimal Amount { get; set; } //setting type/property to decimal
+    }
+}
---
+++ StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number num = new Number(); //creates object from the class
+            num.Amount = 123.45m; //gives property a value
+            Console.WriteLine("Amount:  " + num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
